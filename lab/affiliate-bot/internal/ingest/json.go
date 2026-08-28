de6b18c7b38bc29fb0798dc26219fc7955ad4844
Đinh Hà <hadinhvu@gmail.com>
2026-08-28 15:51:41 +0700

    PR4: add JSON product ingest

---
+++ lab/affiliate-bot/internal/ingest/json.go
@@ -0,0 +1,24 @@
+package ingest
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/dvha85/affiliate-expert-learning-roadmap/lab/affiliate-bot/internal/product"
+)
+
+func ProductsJSON(r io.Reader) ([]product.Product, error) {
+	var products []product.Product
+	dec := json.NewDecoder(r)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(&products); err != nil {
+		return nil, fmt.Errorf("decode products: %w", err)
+	}
+	for i, p := range products {
+		if err := p.Validate(); err != nil {
+			return nil, fmt.Errorf("product[%d]: %w", i, err)
+		}
+	}
+	return products, nil
+}
